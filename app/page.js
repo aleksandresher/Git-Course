effe3db25d0b44b09c2ed7e6316c3cd316e3a21d
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-10-27 23:50:58 +0400

    commit updated files for welcome text

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
+"use client";
 import StartBtn from "./components/startBtn";
 import Card from "./components/card";
 import NextBtn from "./components/nextBtn";
 import PreviousBtn from "./components/previousBtn";
+import WelcomeText from "./components/welcomeText";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex-col">
+        <WelcomeText />
         <StartBtn />
         <Card />
         <div className="flex items-center gap-2 mt-8">
