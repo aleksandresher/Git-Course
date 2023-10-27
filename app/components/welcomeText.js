39abce7069e0fab4d4f02bb1df87938fa7136710
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-10-27 23:43:48 +0400

    add animated welcome text

---
+++ app/components/welcomeText.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { TypeAnimation } from "react-type-animation";
+
+function WelcomeText() {
+  const [textIsVisible, setTextIsVisible] = useState(true);
+  if (!textIsVisible) {
+    return null;
+  } else {
+    return (
+      <div>
+        <TypeAnimation
+          sequence={[
+            // Same substring at the start will only be typed out once, initially
+            "Welcome to our Language Learning Platform!",
+            1000, // wait 1s before replacing "Mice" with "Hamsters"
+            "Explore new languages and expand your horizons.",
+            1000,
+            "Ready to begin your linguistic journey?",
+            1000,
+            "Click 'Start' to embark on your adventure!",
+            1000,
+            () => {
+              setTextIsVisible(false);
+            },
+          ]}
+          wrapper="span"
+          deletionSpeed={80}
+          speed={60}
+          style={{ fontSize: "2em", display: "inline-block" }}
+          repeat="no"
+        />
+      </div>
+    );
+  }
+}
+
+export default WelcomeText;
