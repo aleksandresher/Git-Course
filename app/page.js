6b7893b79b9c62ec16cf7dcd8f53343a1b5d5fa6
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-10-28 21:50:12 +0400

    change background color and start button style

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ import { LanguageCombo } from "./components/languageSelect";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#edede9]">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex-col">
         <WelcomeText />
         <div className="flex justify-center items-center">
