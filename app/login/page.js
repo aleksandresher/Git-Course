c7bdac3e9fd46245626bb760caa146110686092e
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-10-29 20:41:38 +0400

    restructured file organization for signIn/signUp feature

--- app/login/page.js
+++ app/login/page.js
@@ -1,21 +1,21 @@
 "use client";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import LogIn from "../components/logIn";
-import SignUp from "../components/signUp";
+import LogIn from "../components/signIn-Up/logIn";
+import SignUp from "../components/signIn-Up/signUp";
 
 function LoginPage() {
   return (
-    <div className="h-screen w-full flex items-center justify-center bg-[#edede9]">
+    <div className="h-screen w-full flex items-center justify-center bg-[#f0f0ed]">
       <Tabs
         defaultValue="signIn"
         className="w-[400px] h-[420px] bg-[#a2d2ff] p-5 flex flex-col  rounded-md "
       >
         <TabsList className="flex justify-between mb-3">
           <TabsTrigger className="w-1/2" value="signIn">
-            Sign in
+            Sign In
           </TabsTrigger>
           <TabsTrigger className="w-1/2" value="signUp">
-            Sign up
+            Sign Up
           </TabsTrigger>
         </TabsList>
         <TabsContent value="signIn" className="flex flex-col gap-2">
