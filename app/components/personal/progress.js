ccd743e6152906f27b24e1509d5085ec442924c4
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-10-30 15:43:29 +0400

    pregress bar done

---
+++ app/components/personal/progress.js
@@ -0,0 +1,14 @@
+"use client";
+import { Progress } from "@/components/ui/progress";
+import { useState } from "react";
+
+function ProgressBar() {
+  const [progress, setProgress] = useState(90);
+  return (
+    <div className="flex flex-col gap-2">
+      <p>Progress {progress}%</p>
+      <Progress value={progress} />
+    </div>
+  );
+}
+export default ProgressBar;
---
+++ app/user/page.js
@@ -0,0 +1,10 @@
+import ProgressBar from "../components/personal/progress";
+function PersonalPage() {
+  return (
+    <div className="h-screen w-full  bg-[#f0f0ed]">
+      <h1>User Page</h1>
+      <ProgressBar />
+    </div>
+  );
+}
+export default PersonalPage;
