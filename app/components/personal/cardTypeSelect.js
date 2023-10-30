618dd64eb6f20d0890e7ca2415a156db21ba0cd8
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-10-30 16:17:32 +0400

    card selection done

--- app/components/personal/cardTypeSelect.js
+++ app/components/personal/cardTypeSelect.js
@@ -1,28 +1,37 @@
 "use client";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 function CardTypeSelect() {
   return (
-    <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select type of cards" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectItem value="learned">Learned</SelectItem>
-          <SelectItem value="process">In Process</SelectItem>
-          <SelectItem value="difficult">Difficult</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <div>
+      <div className="h-screen w-full flex items-center justify-center bg-[#f0f0ed]">
+        <Tabs
+          defaultValue="signIn"
+          className="w-[400px] h-[420px] bg-[#a2d2ff] p-5 flex flex-col  rounded-md "
+        >
+          <TabsList className="flex justify-between mb-3">
+            <TabsTrigger className="w-1/2" value="learned">
+              Learned
+            </TabsTrigger>
+            <TabsTrigger className="w-1/2" value="difficult">
+              Difficult
+            </TabsTrigger>
+            <TabsTrigger className="w-1/2" value="process">
+              In process
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="learned" className="flex flex-col gap-2">
+            <p>learned cards</p>
+          </TabsContent>
+          <TabsContent value="difficult" className="flex flex-col gap-2">
+            <p>difficult cards</p>
+          </TabsContent>
+          <TabsContent value="process" className="flex flex-col gap-2">
+            <p>in process cards</p>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
   );
 }
 export default CardTypeSelect;
