2316c64c423e74a2cf1e19f3c879a43db0119cfa
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-11-01 20:38:41 +0400

    writing component bug solved

--- app/components/personal/writing.js
+++ app/components/personal/writing.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 function SpellCheck() {
   const [isChecked, setChecked] = useState(false);
   return (
-    <div className="flex  w-full gap-1.5 items-center mt-4 justify-between pb-5">
+    <div className="flex  w-full gap-1.5 items-center  justify-between pb-5 h-[100px]">
       <div>{isChecked && <Input id="word" type="text" />}</div>
       <div className="flex items-center space-x-2">
         <Switch id="spell" checked={isChecked} onCheckedChange={setChecked} />
